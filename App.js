0485effa6b8a47b8fe29351f8148d5afcb473be9
DannyP <kojikusan@gmail.com>
2016-04-26 22:24:35 +0100

    creating init compoments

--- App.js
+++ App.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 class App extends React.Component {
   render(){
-    return <div>Hello world</div>
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <b>Bold</b>
+      </div>
+    );
   }
 }
+
+// const App = () => <h1>Hello world</h1>;
 
 export default App;
